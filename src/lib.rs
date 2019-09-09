7e11ede084b5ca7021bd3c6ebfe2d6997927b753
yumetodo <yume-wikijp@live.jp>
2019-09-09 10:43:24 +0900

    fix(rust): get img alt collectly
    ref:
    - https://github.com/raphlinus/pulldown-cmark/issues/394

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use std::io::{stdout, Write};
-use pulldown_cmark::{ Options, Parser, Event, Tag, CowStr };
+use js_sys::{Error, Function, JsString, TypeError};
+use pulldown_cmark::{CowStr, Event, Options, Parser, Tag};
 use pulldown_cmark_to_cmark::fmt::cmark;
 use wasm_bindgen::prelude::*;
-use js_sys::{JsString, Function, Error, TypeError};
 use wasm_bindgen::JsCast;
 
 #[allow(dead_code)]
@@ -21,7 +20,7 @@ fn example(markdown_input: &str) -> Vec<String> {
             Event::Start(Tag::Image(_, url, _)) => {
                 re.push(url.into_string());
             }
-            _ => ()
+            _ => (),
         }
     }
     // let modified = parser.map(|e| {
@@ -49,13 +48,30 @@ fn example2(markdown_input: &str) -> Vec<String> {
     opts.insert(Options::ENABLE_TASKLISTS);
     let parser = Parser::new_ext(markdown_input, opts);
     let mut re: Vec<String> = Vec::new();
-    for event in parser.clone() {
+    let mut in_image = false;
+    let mut alt: Option<String> = None;
+    for event in parser {
         match event {
-            Event::End(Tag::Image(link_type, url, alt)) => {
-                println!("{:?}, {:?}, {:?}", link_type, url, alt);
-                re.push(alt.into_string());
+            Event::Start(Tag::Image(_, _, _)) => {
+                in_image = true;
             }
-            _ => ()
+            Event::Text(t) => {
+                if alt.is_some() {
+                    let mut tmp = alt.unwrap();
+                    tmp.push(' ');
+                    alt = Some(tmp + &t.into_string());
+                } else if in_image {
+                    alt = Some(t.into_string());
+                }
+            }
+            Event::End(Tag::Image(link_type, u, _)) => {
+                in_image = false;
+                let mut a: Option<String> = None;
+                std::mem::swap(&mut alt, &mut a);
+                println!("{:?}, {:?}, {:?}", link_type, u, a);
+                re.push(a.unwrap());
+            }
+            _ => (),
         }
     }
     re
@@ -71,24 +87,47 @@ impl MarkdownImgUrlEditor {
     #[wasm_bindgen(constructor)]
     pub fn new(text: String, converter: &Function) -> Result<MarkdownImgUrlEditor, JsValue> {
         let markdown_text = text.clone();
-        let parser = Parser::new_ext(&markdown_text, Options::empty());
+        // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+        // and we therefore must enable it explicitly.
+        let mut opts = Options::empty();
+        opts.insert(Options::ENABLE_TABLES);
+        opts.insert(Options::ENABLE_FOOTNOTES);
+        opts.insert(Options::ENABLE_STRIKETHROUGH);
+        opts.insert(Options::ENABLE_TASKLISTS);
+        let parser = Parser::new_ext(&markdown_text, opts);
         let mut string_generators: Vec<Function> = Vec::new();
-        for event in parser.clone() {
+        let mut in_image = false;
+        let mut alt: Option<String> = None;
+        for event in parser {
             match event {
-                Event::Start(Tag::Image(_, u, t)) => {
-                    let this = JsValue::NULL;
-                    let alt = JsValue::from(t.into_string());
+                Event::Start(Tag::Image(_, _, _)) => {
+                    in_image = true;
+                }
+                Event::Text(t) => {
+                    if alt.is_some() {
+                        let mut tmp = alt.unwrap();
+                        tmp.push(' ');
+                        alt = Some(tmp + &t.into_string());
+                    } else if in_image {
+                        alt = Some(t.into_string());
+                    }
+                }
+                Event::End(Tag::Image(_, u, _)) => {
+                    in_image = false;
+                    let mut a: Option<String> = None;
+                    std::mem::swap(&mut alt, &mut a);
+                    let alt = JsValue::from(a.unwrap());
                     let url = JsValue::from(u.into_string());
-                    let generator = converter.call2(&this, &alt, &url);
+                    let generator = converter.call2(&JsValue::NULL, &alt, &url);
                     match generator {
-                        Ok(maybe_g) => {
-                            match maybe_g.dyn_into::<Function>() {
-                                Ok(g) => {
-                                    string_generators.push(g);
-                                },
-                                Err(_) => {
-                                    return Err(JsValue::from(TypeError::new("`converter` (2nd argument): expected Function")));
-                                }
+                        Ok(maybe_g) => match maybe_g.dyn_into::<Function>() {
+                            Ok(g) => {
+                                string_generators.push(g);
+                            }
+                            Err(_) => {
+                                return Err(JsValue::from(TypeError::new(
+                                    "`converter` (2nd argument): expected Function",
+                                )));
                             }
                         },
                         Err(m) => {
@@ -96,7 +135,7 @@ impl MarkdownImgUrlEditor {
                         }
                     }
                 }
-                _ => ()
+                _ => (),
             }
         }
         Ok(MarkdownImgUrlEditor {
@@ -109,23 +148,32 @@ impl MarkdownImgUrlEditor {
         if self.string_generators.is_empty() {
             return Ok(self.markdown_text.clone());
         }
-        let urls = self.string_generators.clone().into_iter().map(
-            |g| g.call0(&JsValue::NULL).and_then(
-                |s| s.dyn_into::<JsString>().map(|s| String::from(s)).map_err(
-                    |_| JsValue::from(TypeError::new("before_collect_callback (3rd argument: expected Function"))
-                )
-            )
-        ).collect::<Result<Vec<String>, JsValue>>()?;
+        let urls = self
+            .string_generators
+            .clone()
+            .into_iter()
+            .map(|g| {
+                g.call0(&JsValue::NULL).and_then(|s| {
+                    s.dyn_into::<JsString>()
+                        .map(|s| String::from(s))
+                        .map_err(|_| {
+                            JsValue::from(TypeError::new(
+                                "before_collect_callback (3rd argument: expected Function",
+                            ))
+                        })
+                })
+            })
+            .collect::<Result<Vec<String>, JsValue>>()?;
         let parser = Parser::new_ext(&self.markdown_text, Options::empty());
         let mut ite = urls.iter();
-        let modified = parser.map(|e| {
-            match e {
-                Event::End(tag) => Event::End(match tag {
-                    Tag::Image(link_type, _, title) => Tag::Image(link_type, CowStr::from(ite.next().unwrap().clone()), title),
-                    _ => tag,
-                }),
-                _ => e,
-            }
+        let modified = parser.map(|e| match e {
+            Event::End(tag) => Event::End(match tag {
+                Tag::Image(link_type, _, title) => {
+                    Tag::Image(link_type, CowStr::from(ite.next().unwrap().clone()), title)
+                }
+                _ => tag,
+            }),
+            _ => e,
         });
         let mut buf = String::with_capacity(self.initial_capacity);
         cmark(modified, &mut buf, None).map_err(|_| Error::new("cmark failed."))?;
@@ -153,7 +201,13 @@ mod tests {
 ```
 ";
         let re = super::example(markdown_input);
-        let test = re.iter().eq(&["1.png", "D3T3cG6U0AAd0Zn.jpg", "D5iuwp0W4AEm1bi.jpg", "D5jwkn7XsAABJvV.jpg", "IR.jpg"]) ;
+        let test = re.iter().eq(&[
+            "1.png",
+            "D3T3cG6U0AAd0Zn.jpg",
+            "D5iuwp0W4AEm1bi.jpg",
+            "D5jwkn7XsAABJvV.jpg",
+            "IR.jpg",
+        ]);
         assert!(test);
     }
     #[test]
@@ -175,7 +229,9 @@ mod tests {
 ";
         let re = super::example2(markdown_input);
         print!("{:?}", re);
-        let test = re.iter().eq(&["1", "cpp", "atgtheiwa1", "atgtheiwa2", "IR"]) ;
+        let test = re
+            .iter()
+            .eq(&["1", "cpp", "atgtheiwa1", "atgtheiwa2", "IR"]);
         assert!(test);
     }
 }
